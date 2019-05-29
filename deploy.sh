6250f331bfe02ce8a72a4a45a77c16c7c5f4f84b
Israel Yshara Garcia <iyshara@luzdelsur.com.pe>
2019-05-29 11:19:29 -0500

    deploy project

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,3 @@ git add .
 git commit -m 'deploy project'
 # git remote add origin https://github.com/mitsuoysharag/sddsad.git
 git push -u origin master
-
-# Deploy Page
-npm run build
-cd dist
-git init
-git add -A
-git commit -m 'deploy page'
-git push -f https://github.com/mitsuoysharag/UNMSM_SD_Pilar_de_Descanso.git  master:gh-pages
-
-cd -
