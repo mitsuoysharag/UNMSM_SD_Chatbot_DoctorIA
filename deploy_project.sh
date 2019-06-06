32be9f7e1655127511e69fd4bb112143010405de
unknown <mitsuoysharag@gmail.com>
2019-06-06 00:26:33 -0500

    deploy project

--- deploy_project.sh
+++ deploy_project.sh
@@ -4,5 +4,6 @@ git init
 # git add README.md
 git add .
 git commit -m 'deploy project'
-# git remote add origin https://github.com/mitsuoysharag/sddsad.git
+# git remote rm origin
+# git remote add origin https://github.com/mitsuoysharag/UNMSM_SD_Pilar_de_Descanso.git
 git push -u origin master
